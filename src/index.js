053844ffcd3bb271c9e00aab5e6113d8c45c59f5
Luiz Silva <lfdjs0000@gmail.com>
2024-01-14 19:45:20 -0300

    Atualização

--- src/index.js
+++ src/index.js
@@ -11,9 +11,9 @@ sequelize.sync().then(() => console.log("Banco de dados conectado com sucesso"))
 
 // Inicializando cors antes das outras funções
 app.use((req, res, next) => {
-	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
+    //Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
     res.header("Access-Control-Allow-Origin", "*");
-	//Quais são os métodos que a conexão pode realizar na API
+    //Quais são os métodos que a conexão pode realizar na API
     res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
     app.use(cors());
     next();
